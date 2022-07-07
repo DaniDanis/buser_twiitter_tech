d43af09087b99241386b029f43e129fe90a60070
Daniel Dantas da Silva <daniel.dantas.98@gmail.com>
2022-07-07 13:46:02 -0300

    Alterado o nome da base.html para pagina_login.html

--- clonnado_twiter/settings.py
+++ clonnado_twiter/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--p@9)%0f#t-)it-^6ev4p7#0(7=x#z+o*wt2d2ov+3jllav7th'
+SECRET_KEY = os.getenv('CURSOS_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
