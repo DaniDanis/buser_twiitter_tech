6196546977534081575b8a70b01360181834cabb
Ronald Perdigão <git.ronaldseabra@gmail.com>
2022-07-15 16:29:40 -0300

    Alterado para, mesmo sem posts, não quebrar o servidor

--- pagina_web/views.py
+++ pagina_web/views.py
@@ -23,9 +23,24 @@ def base(request):
 
 # RONALD~ mexido para pegar os dados do post
 def home(request):
+    url = requests.get("https://newsapi.org/v1/articles?country=br&source=bbc-news&sortBy=top&apiKey=f3ddd76e328349ce8967b46f7703dfad")
+    
+    text = url.text
+    data = json.loads(text)
+    if data['status'] != 'error':
+        article = data['articles']
+    else:
+        article = "............................................................................."  
+    if len(Posts.objects.all()) >= 17:
+        n = 17
+    else:
+        n = len(Posts.objects.all())
+        
     context = {
-        'Posts': Posts.objects.all().order_by('-date'),
-        'form_texto_post': form_TextoPost(),     
+        'numero_de_posts' : n,
+        'Posts': Posts.objects.all().order_by('-date')[:n],
+        'form_texto_post': form_TextoPost(), 
+        'articles': article,    
     }
     if request.method == 'POST':
         user= User.objects.get(id = request.user.id)
@@ -35,14 +50,6 @@ def menubar(request):
     return render(request, 'home/menubar.html', {})
 
 def sidebar(request):
-    url = requests.get("https://newsapi.org/v1/articles?country=br&source=bbc-news&sortBy=top&apiKey=f3ddd76e328349ce8967b46f7703dfad")
-    
-    text = url.text
-    data = json.loads(text)
-    if data['status'] != 'error':
-        article = data['articles']
-    else:
-        article = "............................................................................."  
     return render(request, 'home/sidebar.html', context = {"articles" : article})
 
 def explorar(request):
