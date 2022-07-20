6a33b16a8d6b978987f6dfd064a0f9da4822f2da
Ronald-ps <104438755+Ronald-ps@users.noreply.github.com>
2022-07-19 23:06:40 -0300

    criado functions , e foi repassada para lá a função de nome sidebar (#42)
    criado funcao que limita numero de posts
    criado funcao que verifica se o método da home é post e, se for, salva o post
    adiciona if else python para alterar a classe da imgem de curtir caso ela esteja ou nao na postlike do user
    adicionado java script que seleciona o src da imagem de acordo com a classe ja-curtido. se tiver, ele coloca o coração

---
+++ pagina_web/functions.py
@@ -0,0 +1,48 @@
+from multiprocessing import context
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from pagina_web.models import *
+from pagina_web.forms import form_TextoPost
+from django.http import HttpRequest
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from pagina_web.functions import *
+import json
+import requests
+
+# funcao que importa dados da API DE NOTICIA
+def sidebar(url):
+    url = requests.get(url)
+    
+    text = url.text
+    data = json.loads(text)
+    if data['status'] != 'error':
+        article = data['articles']
+    else:
+        article = "............................................................................."  
+    
+    return article
+
+# retorna o NÚMERO de ~POSTS que pode ter na página
+def limite_posts(lista_de_objetos_post):
+    if len(lista_de_objetos_post) >= 17:
+        n = 17
+    else:
+        n = len(lista_de_objetos_post)
+    return n+1
+
+# verifica se é um ~POST e se for salva no ~BANCO DE DADOS
+def verifica_se_eh_post_e_salva(request, banco_user, banco_posts):
+    if request.method == 'POST':
+        user= banco_user.objects.get(id = request.user.id)
+        banco_posts.objects.create(user=user,texto = request.POST['text-input'] ).save()
+        
+# Retorna lista do id todos os POSTS curtidos pelo usuário
+def retorna_lista_de_posts_curtidos(request, banco_PostLike, ):
+   lista_de_posts = [] 
+   for i in banco_PostLike.objects.filter(user_id = request.user.id):
+        lista_de_posts.append(i.post)
+   return  lista_de_posts
+
