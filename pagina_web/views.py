f1ced82676d9da13a088ec0af7dbf6e8115c9fd9
Ronald Perdigão <git.ronaldseabra@gmail.com>
2022-07-22 17:56:50 -0300

    Criado caixa de comentário , que falta ter efeito onchange pra sumir

--- pagina_web/views.py
+++ pagina_web/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from audioop import reverse
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from .models import *
 from .forms import form_TextoPost
@@ -10,6 +11,8 @@ from django.contrib.auth.decorators import login_required
 import json
 import requests
 from .functions import *
+from django.urls.base import reverse
+from django.db.models import Q
 
 
 # Create your views here.
@@ -24,14 +27,16 @@ def home(request):
     
     # url do bing noticias
     article = sidebar("https://api.bing.microsoft.com/v7.0/news/search")
-    n = limite_posts(Posts.objects.all())
+    posts = Posts.objects.filter(is_comment__isnull = True)
+    n = limite_posts(posts)
     posts_curtidos = retorna_lista_de_posts_curtidos(request, banco_PostLike=PostLike)
     context = {
         'numero_de_posts' : n,
-        'Posts': Posts.objects.all().order_by('-date')[:n],
+        'Posts': posts.order_by('-date')[:n],
         'form_texto_post': form_TextoPost(),
         'articles': article, 
         'posts_curtidos' : posts_curtidos,
+        'post_original': False
            
     }
     verifica_se_eh_post_e_salva(request, banco_user=User, banco_posts=Posts)
@@ -50,4 +55,32 @@ def login(request):
 def curtir_action(request):
    crud_postlike(request)
    return JsonResponse({})
+
+def tocomment(request):
+    texto = request.POST['text-input']
+    user = request.user 
+    post = Posts.objects.get(id = request.POST['id_post'])
+    Posts.objects.create(user=user, texto=texto, is_comment = post).save()
+    return redirect(reverse("home"))
+
+
+def postdetails(request, post_id):
+    # url do bing noticias
+    article = sidebar("https://api.bing.microsoft.com/v7.0/news/search")
+    posts = Posts.objects.filter(is_comment=Posts.objects.get(id = post_id) ).order_by("-date")
+    n = limite_posts(posts)
+    posts_curtidos = retorna_lista_de_posts_curtidos(request, banco_PostLike=PostLike)
+    
+    context = {
+        'numero_de_posts' : n,
+        'Posts': posts.order_by('-date')[:n],
+        'form_texto_post': form_TextoPost(),
+        'articles': article, 
+        'posts_curtidos' : posts_curtidos,
+        'post_original': Posts.objects.get(id = post_id)  
+    }
+    verifica_se_eh_post_e_salva(request, banco_user=User, banco_posts=Posts)
+    context['form_texto_post']: form_TextoPost()
+    return render(request,'home/home.html',context)
+
    
