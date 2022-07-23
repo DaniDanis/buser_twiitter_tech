c7882fc6fea6e4cb7adac1d59ab609dfd86dcb01
Ronald-ps <104438755+Ronald-ps@users.noreply.github.com>
2022-07-22 21:04:49 -0300

    comentario e details (#57)
    * criado functions , e foi repassada para lá a função de nome sidebar
    criado funcao que limita numero de posts
    criado funcao que verifica se o método da home é post e, se for, salva o post
    adiciona if else python para alterar a classe da imgem de curtir caso ela esteja ou nao na postlike do user
    adicionado java script que seleciona o src da imagem de acordo com a classe ja-curtido. se tiver, ele coloca o coração
    
    * criado metodo de envio de dados javascrpt. rederizaçao que faz botao de post ficar preto com clique.
    
    * Criado insercao de like no banco de dados
    
    * :w
    
    * Ajustada chave API de notícias
    
    * Resolvido problema de permissão negada da API de notícias
    
    * javascript adaptado para passar os dados de id do post pelo body do post
    
    * Preparando caixa de comentário
    
    * Criado caixa de comentário , que falta ter efeito onchange pra sumir
    
    * comentarios
    
    Co-authored-by: Marcelo Silva <45953439+mhenrique94@users.noreply.github.com>

--- pagina_web/urls.py
+++ pagina_web/urls.py
@@ -25,5 +25,7 @@ urlpatterns = [
     path('login/', views.login, name="login"),
     path("sidebar/", views.sidebar, name='sidebar'),
     path('', RedirectView.as_view(url='home')),
-    path('curtir-action',views.curtir_action, name="curtir-action"),
+    path('curtir-action/',views.curtir_action, name="curtir-action"),
+    path('tocomment/', views.tocomment,  name="paginaweb.tocomment"),
+    path('details/<int:post_id>/', views.postdetails, name="paginaweb.post_details"),
 ]
