1465ea94a6b3520a24b0d093df4323c907aa5575
root <root@pop-os.localdomain>
2022-07-25 13:54:22 -0300

    Ajustado z-index do wrapper principal

--- pagina_web/static/script/home.js
+++ pagina_web/static/script/home.js
@@ -2,3 +2,11 @@ function auto_height(elem) {  /* javascript */
     elem.style.height = "1px";
     elem.style.height = (elem.scrollHeight)+"px";
 }
+
+function tweet_input_popup() {
+    var display = document.getElementsByClassName('main-textbox-column');
+    if(display[0].style.display == "none")
+        display[0].style.display = 'block';
+    else
+    display[0].style.display = 'none';
+}
