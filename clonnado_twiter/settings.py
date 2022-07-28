1ccc06acb1dcf559101b175ae0070b3c5c20bb81
Daniel Dantas da Silva <daniel.dantas.98@gmail.com>
2022-07-28 11:02:37 -0300

    Deletados algumas linhas de codigo inutil

--- clonnado_twiter/settings.py
+++ clonnado_twiter/settings.py
@@ -39,8 +39,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'pagina_web',
     'contas',
+    'pagina_web',    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
