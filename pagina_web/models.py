2d42dbad1dcd87ba3395d1ad0f577f8e91cb7da0
DaniDanis <104365695+DaniDanis@users.noreply.github.com>
2022-07-28 10:27:10 -0300

    Atualizando o projeto no django_heroku (#66)
    * Adicionado modelo de noticias para popular banco de dados com a API Bing
    
    * Adicionado try/except para possíveis falhas da API
    
    * Ajustado design de foto de perfil e barra do usuário
    
    * Ajustada foto principal para nova estrutura de perfil no backend
    
    * Caixa de comentario melhor rederizada. Post origin dinamizado (#65)
    
    Co-authored-by: Marcelo Silva <45953439+mhenrique94@users.noreply.github.com>
    Co-authored-by: Ronald-ps <104438755+Ronald-ps@users.noreply.github.com>

--- pagina_web/models.py
+++ pagina_web/models.py
@@ -15,5 +15,12 @@ class PostLike(models.Model):
     post = models.ForeignKey(Posts, on_delete=models.CASCADE, related_name="likes")
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="likes")
 
-
+class Noticias(models.Model):
+    autor = models.CharField(max_length=50, blank=False)
+    titulo = models.CharField(max_length=240,blank=False)
+    descricao = models.CharField(max_length=500, blank=False)
+    capa = models.URLField(max_length=1024, null=True, blank=True)
+    
+    def __str__(self):
+        return self.titulo
   
