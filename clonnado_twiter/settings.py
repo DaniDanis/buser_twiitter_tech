8a21cad765879feb1b4ad418bf0d80dccd46c1fa
Daniel Dantas da Silva <daniel.dantas.98@gmail.com>
2022-07-29 16:03:07 -0300

    Aloo

--- clonnado_twiter/settings.py
+++ clonnado_twiter/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 subscription_key = os.getenv('subscription_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
