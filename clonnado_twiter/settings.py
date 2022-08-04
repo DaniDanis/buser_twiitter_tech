8c23393d170c3fd00ead77ecbe8ba4b839116ccc
DaniDanis <104365695+DaniDanis@users.noreply.github.com>
2022-08-04 15:14:43 -0300

    Update settings.py

--- clonnado_twiter/settings.py
+++ clonnado_twiter/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -144,6 +144,6 @@ MEDIA_ROOT = (
   os.path.join(BASE_DIR, "media") #pasta media para abrigar os arquivos dos usuários
 )
 
-MEDIA_URL = 'media/' #endereço para acessar os arquivos
+MEDIA_URL = '/media/' #endereço para acessar os arquivos
 
 django_on_heroku.settings(locals())
